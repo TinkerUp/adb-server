6ee801772d07d52649e9cb8bb401ca8e7d1fec8a
Yahoo Warraich <yhshafqat@gmail.com>
2025-08-25 04:45:57 +0500

    Pulled out the accessgroups to go about it in a normalized way

--- types/models/db.go
+++ types/models/db.go
@@ -1,16 +1,20 @@
 package models
 
 type FileIndex struct {
-	ID           string   `json:"id"`
-	Size         int64    `json:"size"`
-	Owner        string   `json:"owner"`
-	FilePath     string   `json:"file_path"`
-	Checksum     string   `json:"checksum"`
-	CreatedAt    int64    `json:"created_at"`
-	AccessGroups []string `json:"access_groups"`
+	ID        string `json:"id"`
+	Size      int64  `json:"size"`
+	Owner     string `json:"owner"`
+	FilePath  string `json:"file_path"`
+	Checksum  string `json:"checksum"`
+	CreatedAt int64  `json:"created_at"`
 }
 
 type File struct {
 	Metadata FileIndex
 	Data     []byte
+}
+
+type AccessGroupEntry struct {
+	FileId  string `json:"file_id"`
+	GroupId string `json:"group_id"`
 }
